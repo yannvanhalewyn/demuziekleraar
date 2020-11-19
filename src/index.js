25f0dca42510390e8d5ec5e6f50e3cbe6672a1db
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-11-19 15:30:10 +0100

    Toggle between adult and minors pricing

--- src/index.js
+++ src/index.js
@@ -12,3 +12,29 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     }
   });
 });
+
+const setClass = (el, className, enabled) => {
+  if (enabled) {
+    el.classList.add(className);
+  } else {
+    el.classList.remove(className);
+  }
+};
+
+const each = (coll, f) => {
+  for (let x of coll) {
+    f(x);
+  }
+};
+
+document.getElementById("js-pricing-toggle").addEventListener("change", (e) => {
+  const childrenLabel = document.getElementById("js-pricing-label-children");
+  const adultLabel = document.getElementById("js-pricing-label-adults");
+  const childrenPrices = document.getElementsByClassName("js-price-children");
+  const adultPrices = document.getElementsByClassName("js-price-adults");
+
+  setClass(childrenLabel, "pricing-toggle__label--active", !e.target.checked);
+  setClass(adultLabel, "pricing-toggle__label--active", e.target.checked);
+  each(childrenPrices, (p) => setClass(p, "hidden", e.target.checked));
+  each(adultPrices, (p) => setClass(p, "hidden", !e.target.checked));
+});
