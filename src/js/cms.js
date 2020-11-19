68d66e38bf59dbb574bd003251af6cd47952f73c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-11-19 19:14:42 +0100

    Preview lessons in CMS

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,11 +3,12 @@ import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
-
 import HomePreview from "./cms-preview-templates/home";
 import ContactPreview from "./cms-preview-templates/contact";
+import LessonsPreview from "./cms-preview-templates/lessons";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
+CMS.registerPreviewTemplate("lessons", LessonsPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
