60785f5ce33eda07af0014f0a9f136ec1c2d336f
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-11-20 07:40:47 +0100

    Implement pricing page in next js

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,52 @@ export default function Components(props) {
     <header className="flex flex-col md:flex-row justify-between items-center m-container">
       <div className="flex-1 hidden md:block">
         <nav className="pr-4 lg:pr-12 flex justify-between font-medium text-blue-500">
-          <a className="hover:font-semibold" href="#lessons">Muziekles</a>
-          <a className="hover:font-semibold" href="#pricing">Tarieven</a>
-          <a className="hover:font-semibold" href="#contact">Contact</a>
+          <a className="hover:font-semibold" href="#lessons">
+            Muziekles
+          </a>
+          <a className="hover:font-semibold" href="#pricing">
+            Tarieven
+          </a>
+          <a className="hover:font-semibold" href="#contact">
+            Contact
+          </a>
         </nav>
       </div>
 
       <a className="flex-1 inline-block text-center" href="#">
-        <img src="/img/logo.svg" className="w-72 md:w-56 mx-auto" alt="De Muziekleraar logo"/>
+        <img
+          src="/img/logo.svg"
+          className="w-72 md:w-56 mx-auto"
+          alt="De Muziekleraar logo"
+        />
       </a>
 
       <div className="flex-1 text-right">
-        <a href="{{ .Site.Data.socials.youtubeURL }}" target="_blank" className="inline-block bg-gray-900 hover:bg-gray-800 mt-4 md:mt-0 px-4 py-2 text-white text-xs font-bold rounded-md ">
+        <a
+          href="{props.youtubeURL}"
+          target="_blank"
+          className="inline-block bg-gray-900 hover:bg-gray-800 mt-4 md:mt-0 px-4 py-2 text-white text-xs font-bold rounded-md "
+        >
           <div className="flex items-center">
-            <svg className="h-6" viewBox="0 -77 512.00213 512" xmlns="http://www.w3.org/2000/svg">
-              <path d="m501.453125 56.09375c-5.902344-21.933594-23.195313-39.222656-45.125-45.128906-40.066406-10.964844-200.332031-10.964844-200.332031-10.964844s-160.261719 0-200.328125 10.546875c-21.507813 5.902344-39.222657 23.617187-45.125 45.546875-10.542969 40.0625-10.542969 123.148438-10.542969 123.148438s0 83.503906 10.542969 123.148437c5.90625 21.929687 23.195312 39.222656 45.128906 45.128906 40.484375 10.964844 200.328125 10.964844 200.328125 10.964844s160.261719 0 200.328125-10.546875c21.933594-5.902344 39.222656-23.195312 45.128906-45.125 10.542969-40.066406 10.542969-123.148438 10.542969-123.148438s.421875-83.507812-10.546875-123.570312zm0 0" fill="#f00"/>
-              <path d="m204.96875 256 133.269531-76.757812-133.269531-76.757813zm0 0" fill="#fff"/>
+            <svg
+              className="h-6"
+              viewBox="0 -77 512.00213 512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="m501.453125 56.09375c-5.902344-21.933594-23.195313-39.222656-45.125-45.128906-40.066406-10.964844-200.332031-10.964844-200.332031-10.964844s-160.261719 0-200.328125 10.546875c-21.507813 5.902344-39.222657 23.617187-45.125 45.546875-10.542969 40.0625-10.542969 123.148438-10.542969 123.148438s0 83.503906 10.542969 123.148437c5.90625 21.929687 23.195312 39.222656 45.128906 45.128906 40.484375 10.964844 200.328125 10.964844 200.328125 10.964844s160.261719 0 200.328125-10.546875c21.933594-5.902344 39.222656-23.195312 45.128906-45.125 10.542969-40.066406 10.542969-123.148438 10.542969-123.148438s.421875-83.507812-10.546875-123.570312zm0 0"
+                fill="#f00"
+              />
+              <path
+                d="m204.96875 256 133.269531-76.757812-133.269531-76.757813zm0 0"
+                fill="#fff"
+              />
             </svg>
 
-            <span className="ml-2">
-              Kijk op YouTube
-            </span>
+            <span className="ml-2">Kijk op YouTube</span>
           </div>
         </a>
       </div>
     </header>
-  )
+  );
 }
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { markdownToHtml } from "../markdown";
 import SignupButton from "./signup_button";
 
-export default (props) => {
-
-  const { entry } = props;
-  const [ toggled, setToggled ] = useState(false);
+export default function Pricing({ pricingModels }) {
+  // Used in netlify cms preview, sinds we don't hydrate our production app for
+  // page loads there is a native js toggle implementation loaded along the app.
+  const [toggled, setToggled] = useState(false);
 
   return (
     <div className="bg-blue-700 text-white">
@@ -23,7 +23,9 @@ export default (props) => {
             <label
               htmlFor="pricing-toggle"
               id="js-pricing-label-children"
-              className={`pricing-toggle__label ${!toggled ? "pricing-toggle__label--active" : ""}`}
+              className={`pricing-toggle__label ${
+                !toggled ? "pricing-toggle__label--active" : ""
+              }`}
             >
               Tot 21 jaar
             </label>
@@ -42,25 +44,27 @@ export default (props) => {
             <label
               htmlFor="pricing-toggle"
               id="js-pricing-label-adults"
-              className={`pricing-toggle__label ${toggled ? "pricing-toggle__label--active" : ""}`}
+              className={`pricing-toggle__label ${
+                toggled ? "pricing-toggle__label--active" : ""
+              }`}
             >
               Vanaf 21 jaar
             </label>
           </div>
 
           <div className="mt-6 lg:flex lg:bg-blue-600 lg:rounded-xl lg:overflow-hidden lg:shadow-xl">
-            {entry.getIn(["data", "pricingModels"]).map((pricingModel, i) => {
+            {pricingModels.map((pricingModel, i) => {
               return (
                 <div
                   key={i}
-                  className="lg:flex-1 p-6 xs:p-10 md:p-16 rounded-xl shadow-xl bg-blue-600 lg:rounded-none lg:shadow-none lg:bg-none {{ if (lt (add $idx 1) $modelsCount) }} mb-12 lg:mb-0 lg:border-r-2 lg:border-blue-300 {{ end }}"
+                  className={`lg:flex-1 p-6 xs:p-10 md:p-16 rounded-xl shadow-xl bg-blue-600 lg:rounded-none lg:shadow-none lg:bg-none ${i !== pricingModels.length -1 ? "mb-12 lg:mb-0 lg:border-r-2 lg:border-blue-300" : ""}`}
                 >
                   <div className="text-center">
                     <div
                       className="w-64 h-32 md:w-72 md:h-40 mx-auto bg-no-repeat bg-contain bg-center"
                       style={{
                         backgroundImage: `url('${
-                          pricingModel.get("name") === "Groepsles"
+                          pricingModel.name === "Groepsles"
                             ? "/img/community.svg"
                             : "/img/professor.svg"
                         }')`,
@@ -69,15 +73,13 @@ export default (props) => {
 
                     <div className="-mt-12">
                       <h2 className="inline-block px-5 py-1 bg-blue-300 rounded-full heading-3 text-blue-700 font-bold uppercase">
-                        {pricingModel.get("name")}
+                        {pricingModel.name}
                       </h2>
                     </div>
 
                     <div className="flex justify-around">
                       <span className="flex items-center mt-2 text-blue-300 text-xs">
-                        <span className="ml-1">
-                          {pricingModel.get("location")}
-                        </span>
+                        <span className="ml-1">{pricingModel.location}</span>
                       </span>
                     </div>
 
@@ -90,11 +92,13 @@ export default (props) => {
                           €
                         </span>
                         <span className="font-bold text-5xl">
-                          {toggled ? pricingModel.get("priceAdults") : pricingModel.get("priceChildren")}
+                          {toggled
+                            ? pricingModel.priceAdults
+                            : pricingModel.priceChildren}
                         </span>
                       </div>
                       <span className="text-blue-300">
-                        {pricingModel.get("priceSubtitle")}
+                        {pricingModel.priceSubtitle}
                       </span>
                     </div>
 
@@ -103,9 +107,7 @@ export default (props) => {
                     <div
                       className="h-8 mt-4 text-blue-300 strong-white"
                       dangerouslySetInnerHTML={{
-                        __html: markdownToHtml(
-                          pricingModel.get("discount") || ""
-                        ),
+                        __html: markdownToHtml(pricingModel.discount || ""),
                       }}
                     ></div>
                   </div>
@@ -113,7 +115,7 @@ export default (props) => {
                   <hr className="my-8 md:my-12 border-blue-300 border-t-2" />
 
                   <div className="text-blue-300 sm:text-base">
-                    {pricingModel.get("features").map((feature, i) => {
+                    {pricingModel.features.map((feature, i) => {
                       return (
                         <div key={i} className="flex items-center py-3">
                           <div className="w-8 h-8 sm:w-6 sm:h-6">
@@ -124,12 +126,12 @@ export default (props) => {
                               <path d="M10.0933 0.56543C5.04151 0.56543 0.908203 5.06543 0.908203 10.5654C0.908203 16.0654 5.04151 20.5654 10.0933 20.5654C15.1452 20.5654 19.2785 16.0654 19.2785 10.5654C19.2785 5.06543 15.1452 0.56543 10.0933 0.56543ZM13.9511 8.86543L9.54223 13.6654C9.17482 14.0654 8.62371 14.0654 8.25631 13.6654L6.23558 11.4654C5.86817 11.0654 5.86817 10.4654 6.23558 10.0654C6.60298 9.66543 7.15409 9.66543 7.5215 10.0654L8.89927 11.5654L12.6652 7.46543C13.0326 7.06543 13.5837 7.06543 13.9511 7.46543C14.3185 7.86543 14.3185 8.46543 13.9511 8.86543Z" />
                             </svg>
                           </div>
-                          <p
+                          <div
                             className="ml-4 strong-orange-500"
                             dangerouslySetInnerHTML={{
-                              __html: markdownToHtml(feature.get("name")),
+                              __html: markdownToHtml(feature.name),
                             }}
-                          ></p>
+                          ></div>
                         </div>
                       );
                     })}
@@ -142,4 +144,4 @@ export default (props) => {
       </div>
     </div>
   );
-};
+}
