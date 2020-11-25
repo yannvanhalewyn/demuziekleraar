6918c822d87c64f32005bdcc75631e54de6df8fe
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-11-25 22:06:56 +0100

    Fix netlify pull branches
    Banch for previews was: "pull/1/head" instead of the branch name :(

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 const currentGitBranchName = require("current-git-branch");
 
+// Netlify deploys detach the head, but sets the branch variable. Check that
+// one or our current local branch. Used persistence on Github.
+let branch = process.env.BRANCH || currentGitBranchName()
+
+if (branch.startsWith("pull")) {
+  branch = "migrate-to-next";
+}
+
 module.exports = {
   env: {
-    // Netlify deploys detach the head, but sets the branch variable. Check that
-    // one or our current local branch. Used persistence on Github.
-    GIT_BRANCH: process.env.BRANCH || currentGitBranchName()
+    GIT_BRANCH: branch
   }
 };
